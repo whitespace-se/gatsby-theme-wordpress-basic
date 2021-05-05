0c5e1c38cfdd2924e89b8d6b05bbea17e43555e0
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-05 12:04:44 +0200

    feat: add breadcrumbs

--- pkg/src/components/SiteLayout.js
+++ pkg/src/components/SiteLayout.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import PageBreadcrumbs from "./PageBreadcrumbs";
+
 export default function SiteLayout({ children }) {
   return (
     <div>
+      <PageBreadcrumbs />
       <main>{children}</main>
     </div>
   );
