56d0e3d9f97b87cec718de6499e83da56a48a7c3
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-20 10:58:36 +0200

    feat: replace and deprecate Link component

--- pkg/src/theme.js
+++ pkg/src/theme.js
@@ -1,8 +1,16 @@
-import Link from "./components/Link";
+import { Link as GatsbyLink } from "gatsby";
+import React from "react";
+
+function InternalLinkElement({ href, ...restProps }) {
+  return <GatsbyLink to={href} {...restProps} />;
+}
 
 export default {
-  default: {
-    components: { Link },
-  },
+  default: {},
   breadcrumbs: {},
+  link: {
+    components: {
+      InternalLinkElement,
+    },
+  },
 };
