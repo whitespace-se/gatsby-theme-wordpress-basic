75078086aa570c74466bbb07bd4ef936462b5496
Cynthia <cynthia.dao@whitespace.se>
2021-05-20 17:17:08 +0200

    fix: move foundation to correct directory

--- pkg/src/components/Article.js
+++ pkg/src/components/Article.js
@@ -14,7 +14,7 @@ import {
 
 
 import * as defaultStyles from "./Article.module.css";
-import { utilities, layout } from ".";
+import { utilities, layout } from "../foundation";
 
 
 export default withComponentDefaults(Article);
