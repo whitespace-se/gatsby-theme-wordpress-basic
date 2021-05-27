c7c1468db4d4bc67cd9d367b2ec0ea4708392ee5
Fredrik Johansson <jeanfredrik@gmail.com>
2021-05-27 08:49:26 +0200

    refactor!: split Article into subcomponents

--- pkg/src/components/Article.js
+++ pkg/src/components/Article.js
@@ -1,50 +1,47 @@
-import { H, Section } from "@jfrk/react-heading-levels";
+import { Section } from "@jfrk/react-heading-levels";
 import withComponentDefaults from "@whitespace/components/dist/withComponentDefaults";
-
 import clsx from "clsx";
 import React from "react";
 
-import {
-  Image,
-  Time,
-  WPBlocks,
-  BoxNavigation,
-  TextContent,
-} from "../components";
-import { utilities, layout } from "../foundation";
+import { layout } from "../foundation";
+import { usePageContext, useIsFullWidthPage } from "../hooks";
 
 import * as defaultStyles from "./Article.module.css";
+import ArticleBody from "./ArticleBody";
+import ArticleChildPageNav from "./ArticleChildPageNav";
+import ArticleFeaturedImage from "./ArticleFeaturedImage";
+import ArticleMeta from "./ArticleMeta";
+import ArticlePublishDate from "./ArticlePublishDate";
+import ArticleSiblingPageNav from "./ArticleSiblingPageNav";
+import ArticleTagList from "./ArticleTagList";
+import ArticleTitle from "./ArticleTitle";
 
 export default withComponentDefaults(Article);
 
 function Article({
   className,
   styles = defaultStyles,
-  featuredImage,
-  isFullWidthPage,
-  hideTitle,
-  title,
-  pageChildren,
-  pageSiblings,
-  publishedDate,
-  blocksJSON,
-  contentMedia,
-  preamble,
-  content,
-  lastUpdated,
-  managedBy,
-  taxonomies,
-  children,
+  // displayMode,
   ...restProps
 }) {
+  let {
+    contentNode: {
+      id,
+      isFrontPage,
+      contentType: {
+        node: { name: contentTypeName },
+      },
+    },
+    // isPreview,
+  } = usePageContext();
+  let isFullWidthPage = useIsFullWidthPage(id, contentTypeName);
+
   return (
     <article
       className={clsx(layout.component, layout.componentWidthFull, className)}
       {...restProps}
     >
-      {featuredImage && (
-        <Image className={clsx(styles.featuredImage)} {...featuredImage} />
-      )}
+      <ArticleFeaturedImage styles={styles} />
       <div
         className={clsx(
           layout.component,
@@ -53,75 +50,18 @@ function Article({
             : layout.componentWidthNarrow,
         )}
       >
-        <H className={clsx(hideTitle && utilities.visuallyHidden)}>{title}</H>
-        <BoxNavigation className={styles.childPages} items={pageChildren} />
+        <ArticleTitle />
         <Section>
-          {publishedDate && (
-            <div
-              className={clsx(styles.publishedDate)}
-              aria-label={"Publicerad"}
-            >
-              <Time
-                date={publishedDate}
-                format={{
-                  day: "numeric",
-                  month: "long",
-                  year: "numeric",
-                }}
-              />
-            </div>
+          <ArticleChildPageNav />
+          {contentTypeName === "post" && <ArticlePublishDate />}
+          <ArticleBody />
+          {!isFrontPage && (
+            <footer className={styles.meta}>
+              <ArticleMeta />
+              <ArticleTagList taxonomy="tags" />
+            </footer>
           )}
-          <TextContent>
-            {blocksJSON ? (
-              <>
-                <WPBlocks
-                  blocks={JSON.parse(blocksJSON)}
-                  contentMedia={contentMedia}
-                />
-              </>
-            ) : (
-              <>
-                {preamble && (
-                  <div className={clsx(styles.preamble)}>{preamble}</div>
-                )}
-                {content}
-              </>
-            )}
-          </TextContent>
-          <footer>
-            <div className={styles.meta}>
-              {lastUpdated && (
-                <span className={styles.metaTime}>
-                  {"Uppdaterad "}
-                  <Time
-                    date={lastUpdated}
-                    format={{
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    }}
-                  />
-                </span>
-              )}
-              {managedBy && (
-                <span className={styles.metaAuthor}>
-                  {"Sidansvarig: "} {managedBy}
-                </span>
-              )}
-              {taxonomies && (
-                <span className={styles.metaTaxonomies}>
-                  {"Kategorier: "}{" "}
-                  {taxonomies.map((taxonomy) => taxonomy.name).join(", ")}
-                </span>
-              )}
-            </div>
-          </footer>
-          <BoxNavigation
-            className={styles.siblingPages}
-            title="Relaterat innehåll"
-            items={pageSiblings}
-          />
-          {children}
+          {!isFrontPage && <ArticleSiblingPageNav />}
         </Section>
       </div>
     </article>
--- pkg/src/templates/SingleTemplate.js
+++ pkg/src/templates/SingleTemplate.js
@@ -1,58 +1,7 @@
 import React from "react";
 
 import { Article } from "../components";
-import {
-  useHTMLProcessor,
-  usePageChildren,
-  usePageSiblings,
-  useIsFullWidthPage,
-  useIsFrontPage,
-} from "../hooks";
 
 export default function SingleTemplate({ pageContext }) {
-  const {
-    contentNode: {
-      id,
-      title,
-      dateGmt,
-      modifiedGmt,
-      featuredImage,
-      content: contentHTML,
-      contentMedia,
-      blocksJSON,
-      contentArea,
-      contentType: {
-        node: { name: postType },
-      },
-      managedBy: { managedBy },
-      tags: { nodes: tags } = {},
-    },
-    // isPreview,
-  } = pageContext;
-
-  const { processPageContent } = useHTMLProcessor();
-  let { preamble, content } = processPageContent(contentHTML, { contentMedia });
-
-  const articleProps = {
-    featuredImage: !!(featuredImage && featuredImage.node) && {
-      ...featuredImage.node,
-      width: "1025",
-      height: "288",
-    },
-    pageChildren: usePageChildren(id),
-    pageSiblings: usePageSiblings(id),
-    isFullWidthPage: useIsFullWidthPage(id, postType),
-    hideTitle: useIsFrontPage(id),
-    title: title,
-    publishedDate: !useIsFrontPage(id) && dateGmt,
-    blocksJSON: blocksJSON,
-    contentMedia: contentMedia,
-    preamble: !!preamble && preamble,
-    content: content,
-    lastUpdated: !useIsFrontPage(id) && modifiedGmt,
-    managedBy: !useIsFrontPage(id) && managedBy,
-    taxonomies: postType == "post" && tags.length ? [...tags] : false,
-  };
-
-  return <Article {...articleProps} />;
+  return <Article displayMode="full" />;
 }
